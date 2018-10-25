abe73d056a05335078a64f6a5345d5be356d101c
Maciej Jagiellowicz <maciej.jagiellowicz@allegro.pl>
2018-10-25 11:31:40 +0200

    preformance fixes in default_impl

--- lib/graphite_limiter/default_impl.ex
+++ lib/graphite_limiter/default_impl.ex
@@ -4,6 +4,7 @@ defmodule GraphiteLimiter.DefaultImpl do
   @behaviour GraphiteLimiter.Impl
 
   @statsd_prefix Application.get_env(:graphite_limiter, :statsd_prefix, "aggregated")
+  @statsd_prefix_length Application.get_env(:graphite_limiter, :statsd_prefix, 1)
   @path_depth Application.get_env(:graphite_limiter, :metrics_path_depth, 4)
 
 
@@ -18,30 +19,36 @@ defmodule GraphiteLimiter.DefaultImpl do
     |> GraphiteLimiter.Router.validate_metric(metric, sender_pool_size)
   end
 
-  @spec increase_counter(String.t) :: :ok
-  defp increase_counter(path) do
-    Instrumenter.inc_metrics_by_path(path)
+  @spec increase_counter({String.t, integer}) :: :ok
+  defp increase_counter({path, path_length}) do
+    if path_length >= @path_depth do
+      Instrumenter.inc_metrics_by_path(path)
+    end
   end
 
   @spec calculate_path_depth(String.t) :: integer
   defp calculate_path_depth(metric) do
     path_depth = case String.starts_with?(metric, @statsd_prefix) do
-      true -> @statsd_prefix
-              |> String.split(".")
-              |> length
-              |> fn(l) -> l + @path_depth + 1 end.()
+      true ->  @path_depth + @statsd_prefix_length + 1
       false -> @path_depth
     end
     {metric, path_depth}
   end
 
-  @spec build_path({String.t, [String.t]}) :: String.t
+  @spec build_path({String.t, [String.t]}) :: {String.t, integer}
   defp build_path({metric, path_depth}) do
     metric
     |> String.split(" ")
     |> fn([name | _rest]) -> name end.()
-    |> String.split(".")
+    |> String.splitter(".")
     |> Enum.take(path_depth)
-    |> Enum.join(".")
+    |> actual_path_length
+    |> join_parts
   end
+
+  @spec actual_path_length(list(String.t)) :: {list(String.t), integer}
+  defp actual_path_length(metric_parts), do: {metric_parts, length(metric_parts)}
+
+  @spec join_parts({list(String.t), integer}) :: {String.t, integer}
+  defp join_parts({metric_parts, nr_of_parts}), do: {Enum.join(metric_parts, "."), nr_of_parts}
 end
